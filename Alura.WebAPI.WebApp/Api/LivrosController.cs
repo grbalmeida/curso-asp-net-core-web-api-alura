04195c7fae479aa72a00e43ba2aa181fc77041bd
gilvan <g.r.almeida@live.com>
2020-11-18 20:58:07 -0300

    Implementando as outras operações do CRUD

--- Alura.WebAPI.WebApp/Api/LivrosController.cs
+++ Alura.WebAPI.WebApp/Api/LivrosController.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Alura.WebAPI.WebApp.Api
 {
@@ -24,6 +25,57 @@ namespace Alura.WebAPI.WebApp.Api
             }
 
             return Json(model.ToModel());
+        }
+
+        [HttpPost]
+        public ActionResult Incluir(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                _repo.Incluir(livro);
+                var uri = Url.Action(nameof(Recuperar), new { id = livro.Id });
+                return Created(uri, livro);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut]
+        public IActionResult Alterar(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+
+                _repo.Alterar(livro);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            _repo.Excluir(model);
+
+            return NoContent();
         }
     }
 }
